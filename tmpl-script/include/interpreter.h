e854a0466b7c92a55dc6504b642e000f4ed1b7fe
LandaMm <qwer.009771@gmail.com>
2025-01-24 21:20:47 +0100

    feat: moveout env + environment support

--- tmpl-script/include/interpreter.h
+++ tmpl-script/include/interpreter.h
@@ -5,84 +5,26 @@
 #include"node.h"
 #include"node/expression.h"
 #include"node/literal.h"
+#include"node/var_declaration.h"
+#include"interpreter/environment.h"
+#include"interpreter/value.h"
 
 namespace Runtime
 {
 	using namespace AST::Nodes;
 	class Interpreter
 	{
-	public:
-		enum class ValueType
-		{
-			Integer,
-			Float,
-			Double,
-			String
-		};
-		class Value
-		{
-		public:
-			inline virtual ValueType GetType() const = 0;
-		public:
-			template<typename T>
-			inline T* Get() { return static_cast<T*>(this); }
-		};
-		class IntegerValue : public Value
-		{
-		private:
-			std::shared_ptr<int> m_value;
-		public:
-			IntegerValue(std::shared_ptr<int> value) : m_value(value) {}
-			IntegerValue(int value) : m_value(std::make_shared<int>(value)) {}
-		public:
-			inline ValueType GetType() const override { return ValueType::Integer; }
-		public:
-			inline std::shared_ptr<int> GetValue() const { return m_value; }
-		};
-		class FloatValue : public Value
-		{
-		private:
-			std::shared_ptr<float> m_value;
-		public:
-			FloatValue(std::shared_ptr<float> value) : m_value(value) {}
-			FloatValue(float value) : m_value(std::make_shared<float>(value)) {}
-		public:
-			inline ValueType GetType() const override { return ValueType::Float; }
-		public:
-			inline std::shared_ptr<float> GetValue() const { return m_value; }
-		};
-		class DoubleValue : public Value
-		{
-		private:
-			std::shared_ptr<double> m_value;
-		public:
-			DoubleValue(std::shared_ptr<double> value) : m_value(value) {}
-			DoubleValue(double value) : m_value(std::make_shared<double>(value)) {}
-		public:
-			inline ValueType GetType() const override { return ValueType::Double; }
-		public:
-			inline std::shared_ptr<double> GetValue() const { return m_value; }
-		};
-		class StringValue : public Value
-		{
-		private:
-			std::shared_ptr<std::string> m_value;
-		public:
-			StringValue(std::shared_ptr<std::string> value) : m_value(value) {}
-			StringValue(std::string value) : m_value(std::make_shared<std::string>(value)) {}
-		public:
-			inline ValueType GetType() const override { return ValueType::String; }
-		public:
-			inline std::shared_ptr<std::string> GetValue() const { return m_value; }
-		};
 	private:
 		std::shared_ptr<Parser> m_parser;
+		std::shared_ptr<Environment> m_env;
 	public:
-		Interpreter(std::shared_ptr<Parser> parser) : m_parser(parser) {}
+		Interpreter(std::shared_ptr<Parser> parser, std::shared_ptr<Environment> env) : m_parser(parser), m_env(env) {}
 	public:
 		std::shared_ptr<Value> Evaluate(std::shared_ptr<Node> node);
 		std::shared_ptr<Value> EvaluateExpression(std::shared_ptr<ExpressionNode> expr);
+		void EvaluateVariableDeclaration(std::shared_ptr<VarDeclaration> varDecl);
 		std::shared_ptr<Value> EvaluateLiteral(std::shared_ptr<LiteralNode> literal);
+		ValueType EvaluateType(std::shared_ptr<Node> typeNode);
 	};
 }
 
