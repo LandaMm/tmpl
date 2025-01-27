2577e9e7eb1bebcd2ce6d082b3097aa3b058d196
LandaMm <qwer.009771@gmail.com>
2025-01-27 15:30:29 +0100

    fmt: function.h

--- tmpl-script/include/node/function.h
+++ tmpl-script/include/node/function.h
@@ -1,8 +1,8 @@
 #ifndef FUNCTION_CALL_H
 #define FUNCTION_CALL_H
-#include<vector>
-#include<memory>
-#include"../node.h"
+#include <vector>
+#include <memory>
+#include "../node.h"
 
 namespace AST
 {
@@ -13,15 +13,18 @@ namespace AST
 		private:
 			std::shared_ptr<Node> m_callee;
 			std::vector<std::shared_ptr<Node>> m_args;
+
 		private:
 			std::string Format() const override;
 			inline NodeType GetType() const override { return NodeType::FunctionCall; }
+
 		public:
 			FunctionCall(std::shared_ptr<Node> callee, std::vector<std::shared_ptr<Node>> args) : m_callee(callee), m_args(args) {}
 			~FunctionCall() {}
+
 		public:
 			inline std::shared_ptr<Node> GetCallee() const { return m_callee; }
-			inline std::vector<std::shared_ptr<Node>>* GetArgs() { return &m_args; }
+			inline std::vector<std::shared_ptr<Node>> *GetArgs() { return &m_args; }
 		};
 	}
 }
