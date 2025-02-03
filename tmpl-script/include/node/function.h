965915a872469f98adac9af065ab3b34e1c7e4b3
LandaMm <qwer.009771@gmail.com>
2025-02-03 19:53:31 +0100

    fix: shared ptr to function params

--- tmpl-script/include/node/function.h
+++ tmpl-script/include/node/function.h
@@ -49,7 +49,7 @@ namespace AST
         {
         private:
             std::shared_ptr<IdentifierNode> m_name;
-            std::vector<FunctionParam> m_params;
+            std::vector<std::shared_ptr<FunctionParam>> m_params;
             std::shared_ptr<Node> m_ret_type;
             std::shared_ptr<Statements::StatementsBody> m_body;
         private:
@@ -61,13 +61,14 @@ namespace AST
                     Location loc
                     )
                 : m_name(name),
-                m_params(std::vector<FunctionParam>()),
+                m_params(std::vector<std::shared_ptr<FunctionParam>>()),
                 m_ret_type(nullptr),
                 m_body(body),
+                m_index(0),
                 Node(loc) { }
             ~FunctionDeclaration() = default;
         public:
-            void AddParam(FunctionParam param) { m_params.push_back(param); }
+            void AddParam(std::shared_ptr<FunctionParam> param);
             void SetReturnType(std::shared_ptr<Node> retType) { m_ret_type = retType; }
         public:
             inline std::string GetName() const { return m_name->GetName(); }
@@ -75,7 +76,7 @@ namespace AST
             inline std::shared_ptr<Statements::StatementsBody> GetBody() const { return m_body; }
         public: // Iterate
             inline bool HasParams() { return m_params.size() > m_index; }
-            FunctionParam GetNextParam() { return m_params[m_index++]; }
+            std::shared_ptr<FunctionParam> GetNextParam() { return m_params[m_index++]; }
 		public:
 			inline NodeType GetType() const override { return NodeType::FnDecl; }
             std::string Format() const override;
