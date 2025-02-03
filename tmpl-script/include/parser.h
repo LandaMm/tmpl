c5bc703c9522fcf2e9ac3fa3c591c50da883772c
LandaMm <qwer.009771@gmail.com>
2025-02-03 19:55:09 +0100

    feat: export&require stmt parsing h

--- tmpl-script/include/parser.h
+++ tmpl-script/include/parser.h
@@ -55,6 +55,9 @@ namespace AST
 		std::shared_ptr<Node> ProcedureDeclaration();
         std::shared_ptr<Node> ReturnStatement();
         std::shared_ptr<Node> FunctionDeclaration();
+        std::shared_ptr<Node> ExportStmt();
+    private: // Macros
+        std::shared_ptr<Node> RequireStatement();
 	};
 }
 
