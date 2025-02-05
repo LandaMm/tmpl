a72c64ad26f285cb8294f8d8c9fc2cd54422a6bd
LandaMm <qwer.009771@gmail.com>
2025-02-05 19:52:52 +0100

    feat: impl node diagnostics in typechecker

--- tmpl-script/src/typechecker.cpp
+++ tmpl-script/src/typechecker.cpp
@@ -1,6 +1,7 @@
 
 #include <cassert>
 #include <memory>
+#include <filesystem>
 #include "../include/typechecker.h"
 #include "../include/error.h"
 #include "../include/node/return.h"
@@ -8,7 +9,13 @@
 #include "../include/node/statement.h"
 #include "../include/node/var_declaration.h"
 #include "../include/node/function.h"
-#include "include/node/procedure.h"
+#include "../include/interpreter.h"
+#include "../include/node/procedure.h"
+#include "../include/node/unary.h"
+#include "include/interpreter/value.h"
+#include "include/node/logical.h"
+
+namespace fs = std::filesystem;
 
 namespace Runtime
 {
@@ -65,11 +72,30 @@ namespace Runtime
                 return DiagnoseLiteral(std::dynamic_pointer_cast<LiteralNode>(node));
             case NodeType::Identifier:
                 return DiagnoseId(std::dynamic_pointer_cast<IdentifierNode>(node));
+            case NodeType::Unary:
+                return DiagnoseUnary(std::dynamic_pointer_cast<UnaryNode>(node));
             case NodeType::FunctionCall:
                 return DiagnoseFnCall(std::dynamic_pointer_cast<FunctionCall>(node));
             case NodeType::VarDecl:
                 HandleVarDeclaration(std::dynamic_pointer_cast<VarDeclaration>(node));
                 return ValueType::Null;
+            case NodeType::Condition:
+                return DiagnoseCondition(std::dynamic_pointer_cast<Condition>(node));
+            case NodeType::Ternary:
+                return DiagnoseTernary(std::dynamic_pointer_cast<TernaryNode>(node));
+            case NodeType::Return:
+            {
+                auto ret = std::dynamic_pointer_cast<ReturnNode>(node);
+                return DiagnoseNode(ret->GetValue());
+            }
+            case NodeType::IfElse:
+            {
+                auto ifElse = std::dynamic_pointer_cast<Statements::IfElseStatement>(node);
+                DiagnoseNode(ifElse->GetCondition());
+                DiagnoseNode(ifElse->GetBody());
+                if (ifElse->GetElseNode() != nullptr) DiagnoseNode(ifElse->GetElseNode());
+                return ValueType::Null;
+            }
             case NodeType::Block:
             {
                 auto block = std::dynamic_pointer_cast<Statements::StatementsBody>(node);
@@ -86,6 +112,127 @@ namespace Runtime
         }
     }
 
+    ValueType TypeChecker::DiagnoseCondition(std::shared_ptr<Condition> condition)
+    {
+		ValueType left = DiagnoseNode(condition->GetLeft());
+		ValueType right = DiagnoseNode(condition->GetRight());
+
+		if (left != right && left != ValueType::Null && right != ValueType::Null)
+		{
+			Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
+			errorManager.OperandMismatchType(GetFilename(), left, right, condition->GetLocation(), "RuntimeError");
+			return ValueType::Null;
+		}
+
+		if (left != right && (left == ValueType::Null || right == ValueType::Null))
+		{
+			if (condition->GetOperator() != Condition::ConditionType::Compare && condition->GetOperator() != Condition::ConditionType::NotEqual)
+			{
+				Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
+				errorManager.RaiseError("Unsupported condition operator for null values: " + std::to_string((int)condition->GetOperator()));
+				return ValueType::Null;
+			}
+            return ValueType::Bool;
+		}
+
+		return ValueType::Bool;
+    }
+
+    ValueType TypeChecker::DiagnoseTernary(std::shared_ptr<TernaryNode> ternary)
+    {
+		ValueType condition = DiagnoseNode(ternary->GetCondition());
+
+        // TODO: boolean support
+		if (condition != ValueType::Integer)
+		{
+			// should be unreachable
+			Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
+			errorManager.RaiseError("RuntimeError: Condition cannot produce non-integer value");
+			return ValueType::Null;
+		}
+
+        auto leftType = DiagnoseNode(ternary->GetLeft());
+        auto rightType = DiagnoseNode(ternary->GetRight());
+
+        if (leftType != rightType) {
+			Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
+			errorManager.TypeMismatch(GetFilename(), leftType, rightType, ternary->GetLocation());
+			return ValueType::Null;
+        }
+
+		return leftType;
+    }
+
+    void TypeChecker::RunModuleChecker(std::shared_ptr<RequireMacro> require)
+    {
+        std::string module = require->GetModule();
+        fs::path currentPath = GetFilename();
+        fs::path modulePath = currentPath.parent_path() / (module + ".tmpl");
+
+        std::cout << "[DEBUG] Import module from '" << modulePath << "'" << std::endl;
+
+        std::ifstream moduleFile(modulePath);
+        if (!moduleFile.good())
+        {
+            Prelude::ErrorManager& errManager = Prelude::ErrorManager::getInstance();
+            errManager.FailedOpeningFile(modulePath.string());
+            return;
+        }
+
+        std::shared_ptr<Lexer> lexer = std::make_shared<Lexer>(moduleFile, modulePath.string());
+        lexer->Tokenize();
+
+        std::shared_ptr<Parser> parser = std::make_shared<Parser>(lexer);
+        parser->Parse();
+
+        std::shared_ptr<ProgramNode> program = std::dynamic_pointer_cast<ProgramNode>(parser->GetRoot());
+
+        SetFilename(modulePath.string());
+        HandleModule(program);
+        SetFilename(currentPath.string());
+
+    }
+
+    void TypeChecker::HandleExportStatement(std::shared_ptr<ExportStatement> exportStmt)
+    {
+        auto target = exportStmt->GetTarget();
+        switch(target->GetType())
+        {
+           case NodeType::FnDecl:
+                HandleFnDeclaration(std::dynamic_pointer_cast<FunctionDeclaration>(target));
+                break;
+            case NodeType::VarDecl:
+                HandleVarDeclaration(std::dynamic_pointer_cast<VarDeclaration>(target));
+                break;
+            default:
+                assert(false && "Unreachable. Should be handled by parser.");
+                return;
+        }
+
+    }
+
+    void TypeChecker::HandleModule(std::shared_ptr<ProgramNode> program)
+    {
+        while (auto stmt = program->Next())
+        {
+            switch (stmt->GetType())
+            {
+                case NodeType::Require:
+                    RunModuleChecker(std::dynamic_pointer_cast<RequireMacro>(stmt));
+                    break;
+                case NodeType::Export:
+                    HandleExportStatement(std::dynamic_pointer_cast<ExportStatement>(stmt));
+                    break;
+                default:
+                    {
+                        Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
+                        errorManager.RaiseError("Unsupported node type by evaluating imported module: " + std::to_string((int)stmt->GetType()));
+                        break;
+                    }
+            }
+        }
+    }
+
     void TypeChecker::RunChecker(std::shared_ptr<ProgramNode> program)
     {
         while(auto stmt = program->Next())
@@ -99,6 +246,9 @@ namespace Runtime
                     DiagnoseNode(proc->GetBody());
                     break;
                 }
+                case NodeType::Require:
+                    RunModuleChecker(std::dynamic_pointer_cast<RequireMacro>(stmt));
+                    break;
                 case NodeType::FnDecl:
                     HandleFnDeclaration(std::dynamic_pointer_cast<FunctionDeclaration>(stmt));
                     break;
@@ -179,6 +329,17 @@ namespace Runtime
         return fn->GetReturnType();
     }
 
+    ValueType TypeChecker::DiagnoseUnary(std::shared_ptr<UnaryNode> unary)
+    {
+        auto op = unary->GetOperator();
+        if (op == UnaryNode::UnaryOperator::Negative || op == UnaryNode::UnaryOperator::Positive)
+        {
+            return DiagnoseNode(unary->GetTarget());
+        }
+
+        return ValueType::Bool;
+    }
+
     void TypeChecker::AssumeIfElse(std::shared_ptr<Statements::IfElseStatement> ifElse, ValueType expected)
     {
         AssumeBlock(ifElse->GetBody(), expected);
@@ -215,6 +376,10 @@ namespace Runtime
             else if (stmt->GetType() == NodeType::IfElse)
             {
                 AssumeIfElse(std::dynamic_pointer_cast<Statements::IfElseStatement>(stmt), expected);
+            }
+            else
+            {
+                DiagnoseNode(stmt);
             }
         }
 
