3b5ce6004497ad42224b6e1bcf3dffb118473bab
LandaMm <qwer.009771@gmail.com>
2025-02-11 23:25:32 +0100

    feat: define typevariable with complex value type + rewrite all methods return types to complex ones

--- tmpl-script/include/typechecker.h
+++ tmpl-script/include/typechecker.h
@@ -23,13 +23,13 @@ namespace Runtime
     class TypeVariable
     {
     private:
-        ValueType m_type;
+        std::shared_ptr<ComplexValueType> m_type;
         bool m_editable;
     public:
-        TypeVariable(ValueType type, bool editable)
+        TypeVariable(std::shared_ptr<ComplexValueType> type, bool editable)
             : m_type(type), m_editable(editable) { }
     public:
-        inline ValueType GetType() const { return m_type; }
+        inline std::shared_ptr<ComplexValueType> GetType() const { return m_type; }
         inline bool IsEditable() const { return m_editable; }
     };
 
@@ -37,13 +37,13 @@ namespace Runtime
 	{
 	private:
         std::vector<std::shared_ptr<FnParam>> m_params;
-        ValueType m_ret_type;
+        std::shared_ptr<ComplexValueType> m_ret_type;
         std::string m_module_name;
         bool m_exported;
         Location m_loc;
 
 	public:
-		TypeFn(ValueType retType, std::string module, bool exported, Location loc)
+		TypeFn(std::shared_ptr<ComplexValueType> retType, std::string module, bool exported, Location loc)
 			: m_ret_type(retType),
             m_params(std::vector<std::shared_ptr<FnParam>>()),
             m_module_name(module),
@@ -58,7 +58,7 @@ namespace Runtime
             { return m_params[index]; }
         inline size_t GetParamsSize() const { return m_params.size(); }
     public:
-        inline ValueType GetReturnType() const { return m_ret_type; }
+        inline std::shared_ptr<ComplexValueType> GetReturnType() const { return m_ret_type; }
         inline std::string GetModuleName() const { return m_module_name; }
         inline bool IsExported() const { return m_exported; }
         inline Location GetLocation() const { return m_loc; }
@@ -72,7 +72,7 @@ namespace Runtime
         std::shared_ptr<Environment<TypeVariable>> m_variables;
         std::shared_ptr<Environment<TypeFn>> m_functions;
         std::shared_ptr<Environment<std::string>> m_modules;
-        std::shared_ptr<Environment<Environment<TypeFn>, ValueType>> m_type_functions;
+        std::shared_ptr<Environment<Environment<TypeFn>, std::shared_ptr<ComplexValueType>>> m_type_functions;
         int m_errors;
         
     public:
@@ -81,32 +81,32 @@ namespace Runtime
               m_variables(std::make_shared<Environment<TypeVariable>>()),
               m_functions(std::make_shared<Environment<TypeFn>>()),
               m_modules(std::make_shared<Environment<std::string>>()),
-              m_type_functions(std::make_shared<Environment<Environment<TypeFn>, ValueType>>()),
+              m_type_functions(std::make_shared<Environment<Environment<TypeFn>, std::shared_ptr<ComplexValueType>>>()),
               m_errors(0) { }
     public:
         void RunChecker(std::shared_ptr<ProgramNode> program);
         void RunModuleChecker(std::shared_ptr<RequireMacro> require);
     private:
-        ValueType DiagnoseNode(std::shared_ptr<Node> node);
-        ValueType DiagnoseExpression(std::shared_ptr<ExpressionNode> expr);
-        ValueType DiagnoseLiteral(std::shared_ptr<LiteralNode> literal);
-        ValueType DiagnoseId(std::shared_ptr<IdentifierNode> identifier);
-        ValueType DiagnoseFnCall(std::shared_ptr<FunctionCall> fnCall);
-        ValueType DiagnoseUnary(std::shared_ptr<UnaryNode> unary);
-        ValueType DiagnoseCondition(std::shared_ptr<Condition> condition);
-        ValueType DiagnoseTernary(std::shared_ptr<TernaryNode> ternary);
+        std::shared_ptr<ComplexValueType> DiagnoseNode(std::shared_ptr<Node> node); // done
+        std::shared_ptr<ComplexValueType> DiagnoseExpression(std::shared_ptr<ExpressionNode> expr); // done
+        std::shared_ptr<ComplexValueType> DiagnoseLiteral(std::shared_ptr<LiteralNode> literal); // done
+        std::shared_ptr<ComplexValueType> DiagnoseId(std::shared_ptr<IdentifierNode> identifier); // done
+        std::shared_ptr<ComplexValueType> DiagnoseFnCall(std::shared_ptr<FunctionCall> fnCall); // done
+        std::shared_ptr<ComplexValueType> DiagnoseUnary(std::shared_ptr<UnaryNode> unary); // done
+        std::shared_ptr<ComplexValueType> DiagnoseCondition(std::shared_ptr<Condition> condition); // done
+        std::shared_ptr<ComplexValueType> DiagnoseTernary(std::shared_ptr<TernaryNode> ternary); // done
         // TODO:
 
     private:
-        void HandleVarDeclaration(std::shared_ptr<VarDeclaration> varDecl);
-        void HandleFnDeclaration(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported);
-        void HandleFnSignature(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported);
-        void HandleModule(std::shared_ptr<ProgramNode> program);
-        void HandleExportStatement(std::shared_ptr<ExportStatement> exportStmt);
+        void HandleVarDeclaration(std::shared_ptr<VarDeclaration> varDecl); // done
+        void HandleFnDeclaration(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported); // done
+        void HandleFnSignature(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported); // done
+        void HandleModule(std::shared_ptr<ProgramNode> program); // skip
+        void HandleExportStatement(std::shared_ptr<ExportStatement> exportStmt); // skip
 
     private:
-        void AssumeBlock(std::shared_ptr<Statements::StatementsBody> body, ValueType expected);
-        void AssumeIfElse(std::shared_ptr<Statements::IfElseStatement> ifElse, ValueType expected);
+        void AssumeBlock(std::shared_ptr<Statements::StatementsBody> body, std::shared_ptr<ComplexValueType> expected); // done
+        void AssumeIfElse(std::shared_ptr<Statements::IfElseStatement> ifElse, std::shared_ptr<ComplexValueType> expected); // done
 
     private:
         void ReportError() { m_errors++; };
@@ -115,7 +115,7 @@ namespace Runtime
         int GetErrorReport() { return m_errors; }
 
 	public:
-		static ValueType EvaluateType(std::string filename, std::shared_ptr<Node> typeNode);
+		static std::shared_ptr<ComplexValueType> EvaluateType(std::string filename, std::shared_ptr<Node> typeNode); // done
 
     private:
         std::string GetFilename() const { return m_filename; }
