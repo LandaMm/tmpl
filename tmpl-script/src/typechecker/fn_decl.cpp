9cab02ea6359074b6fa5f3ce466e11c9806b2bd6
LandaMm <qwer.009771@gmail.com>
2025-02-11 23:26:00 +0100

    feat: fn_decl rewrite to complex

--- tmpl-script/src/typechecker/fn_decl.cpp
+++ tmpl-script/src/typechecker/fn_decl.cpp
@@ -16,7 +16,7 @@ namespace Runtime
     void TypeChecker::HandleFnDeclaration(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported)
     {
         std::shared_ptr<Statements::StatementsBody> body = fnDecl->GetBody();
-        ValueType retType = EvaluateType(GetFilename(), fnDecl->GetReturnType());
+        std::shared_ptr<ComplexValueType> retType = EvaluateType(GetFilename(), fnDecl->GetReturnType());
 
         std::shared_ptr<Node> nameNode = fnDecl->GetName();
 
@@ -30,7 +30,7 @@ namespace Runtime
         {
             auto param = fnDecl->GetItem(it->GetPosition());
             it->Next();
-            ValueType paramType = EvaluateType(GetFilename(), param->GetType());
+            std::shared_ptr<ComplexValueType> paramType = EvaluateType(GetFilename(), param->GetType());
             std::string paramName = param->GetName()->GetName();
             std::shared_ptr<FnParam> fnParam = std::make_shared<FnParam>(paramType, paramName);
             fn->AddParam(fnParam);
@@ -50,7 +50,7 @@ namespace Runtime
             case AST::NodeType::ObjectMember:
             {
                 auto obj = std::dynamic_pointer_cast<ObjectMember>(nameNode);
-                ValueType targetType = EvaluateType(GetFilename(), obj->GetObject());
+                std::shared_ptr<ComplexValueType> targetType = EvaluateType(GetFilename(), obj->GetObject());
                 if (!m_type_functions->HasItem(targetType))
                     m_type_functions->AddItem(targetType, std::make_shared<Environment<TypeFn>>());
 
@@ -94,7 +94,7 @@ namespace Runtime
 
     void TypeChecker::HandleFnSignature(std::shared_ptr<FunctionDeclaration> fnDecl, bool exported)
     {
-        ValueType retType = EvaluateType(GetFilename(), fnDecl->GetReturnType());
+        std::shared_ptr<ComplexValueType> retType = EvaluateType(GetFilename(), fnDecl->GetReturnType());
 
         std::shared_ptr<Node> nameNode = fnDecl->GetName();
         assert(nameNode->GetType() == NodeType::Identifier && "Name should be identifier");
@@ -107,7 +107,7 @@ namespace Runtime
         {
             auto param = fnDecl->GetItem(it->GetPosition());
             it->Next();
-            ValueType paramType = EvaluateType(GetFilename(), param->GetType());
+            std::shared_ptr<ComplexValueType> paramType = EvaluateType(GetFilename(), param->GetType());
             std::string paramName = param->GetName()->GetName();
             std::shared_ptr<FnParam> fnParam = std::make_shared<FnParam>(paramType, paramName);
             fn->AddParam(fnParam);
