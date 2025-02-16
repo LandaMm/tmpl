52623dd87f334be398bb24112a17fa2a9b5e98a9
LandaMm <qwer.009771@gmail.com>
2025-02-16 16:25:28 +0100

    feat: return val type in evaluate type

--- tmpl-script/src/typechecker/type.cpp
+++ tmpl-script/src/typechecker/type.cpp
@@ -1,58 +1,16 @@
 
 
 #include "../../include/typechecker.h"
+#include "include/interpreter/value.h"
+#include <memory>
 
 namespace Runtime
 {
     using namespace AST::Nodes;
 
-    ValueType TypeChecker::EvaluateType(std::string filename, std::shared_ptr<Node> typeNode)
+    PValType TypeChecker::EvaluateType(std::string filename, std::shared_ptr<TypeNode> typeNode)
     {
-		if (typeNode->GetType() == NodeType::Identifier)
-		{
-			auto id = std::dynamic_pointer_cast<IdentifierNode>(typeNode);
-			if (id->GetName() == "string")
-			{
-				return ValueType::String;
-			}
-			else if (id->GetName() == "double")
-			{
-				return ValueType::Double;
-			}
-			else if (id->GetName() == "float")
-			{
-				return ValueType::Float;
-			}
-			else if (id->GetName() == "int")
-			{
-				return ValueType::Integer;
-			}
-			else if (id->GetName() == "bool")
-            {
-                return ValueType::Bool;
-            }
-			else if (id->GetName() == "void")
-            {
-                return ValueType::Null;
-            }
-			else
-			{
-				Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
-				errorManager.UndefinedType(filename, id->GetName(), id->GetLocation(), "TypeError");
-                // Try reporting instead of exiting
-                exit(-1);
-			}
-		}
-		else
-		{
-			// TODO: support for complex types, e.g. inline objects, generic types
-			Prelude::ErrorManager &errorManager = Prelude::ErrorManager::getInstance();
-			errorManager.RaiseError("Unsupported node type for type: " + std::to_string((int)typeNode->GetType()), "TypeError");
-            // TODO: try reporint instead of exiting
-            exit(-1);
-		}
-
-		return ValueType::Null;
+        return std::make_shared<ValType>(typeNode->GetTypeName()->GetName());
     }
 }
 
