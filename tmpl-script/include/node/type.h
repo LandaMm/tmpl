45735f498ded409ba5b6bc292ee7f256ce4f5a52
LandaMm <qwer.009771@gmail.com>
2025-02-16 13:50:52 +0100

    feat: type node

---
+++ tmpl-script/include/node/type.h
@@ -0,0 +1,33 @@
+
+#ifndef TYPE_NODE_H
+#define TYPE_NODE_H
+
+#include <memory>
+#include "../node.h"
+#include "include/node/identifier.h"
+
+namespace AST
+{
+    namespace Nodes
+    {
+        class TypeNode : public Node
+        {
+        public:
+            using PId = std::shared_ptr<IdentifierNode>;
+        private:
+            std::shared_ptr<IdentifierNode> m_typename;
+            // TODO:
+            // std::vector<std::shared_ptr<...>> m_generics;
+
+        public:
+            TypeNode(PId target, Location loc)
+                : m_typename(target), Node(loc) { }
+
+        public:
+            inline PId GetTypeName() const { return m_typename; }
+        };
+    }
+}
+
+#endif // TYPE_NODE_H
+
