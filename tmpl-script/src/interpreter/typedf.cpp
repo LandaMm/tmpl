171085bc5a4a839cd33ae8f037bbcd0d3e800116
LandaMm <qwer.009771@gmail.com>
2025-02-18 22:43:06 +0100

    feat: pass base type as pvaltype

--- tmpl-script/src/interpreter/typedf.cpp
+++ tmpl-script/src/interpreter/typedf.cpp
@@ -1,5 +1,6 @@
 
 #include "include/interpreter.h"
+#include "include/typechecker.h"
 
 namespace Runtime
 {
@@ -18,7 +19,7 @@ namespace Runtime
         auto baseType = typeDfn->GetTypeValue();
 
         std::string key = typeName->GetTypeName()->GetName();
-        auto typeDf = std::make_shared<TypeDf>(key, baseType->GetTypeName()->GetName());
+        auto typeDf = std::make_shared<TypeDf>(key, TypeChecker::EvaluateType(GetFilename(), baseType));
 
         m_type_definitions->AddItem(key, typeDf);
     }
