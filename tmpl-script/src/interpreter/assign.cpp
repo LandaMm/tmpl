79ba00ec10a945ba068282f3f1e47174b3c74ea9
LandaMm <qwer.009771@gmail.com>
2025-02-23 17:33:25 +0100

    feat: interprete assign operators

--- tmpl-script/src/interpreter/assign.cpp
+++ tmpl-script/src/interpreter/assign.cpp
@@ -1,4 +1,5 @@
 
+#include "include/node/assign.h"
 #include <memory>
 #include "include/interpreter.h"
 #include "include/node/identifier.h"
@@ -35,6 +36,29 @@ namespace Runtime
             Prelude::ErrorManager& errManager = Prelude::ErrorManager::getInstance();
             errManager.AssignMismatchType(GetFilename(), varName, newValue->GetType(), variable->GetType(), assignment->GetLocation(), "RuntimeError");
             return;
+        }
+
+        auto currValue = variable->GetValue()->Clone();
+
+        switch (assignment->GetAssignOp())
+        {
+            case AST::Nodes::AssignOperator::Reassign:
+                break;
+            case AST::Nodes::AssignOperator::Add:
+                newValue = currValue->Operate(newValue, AST::Nodes::ExpressionNode::OperatorType::PLUS);
+                break;
+            case AST::Nodes::AssignOperator::Subtract:
+                newValue = currValue->Operate(newValue, AST::Nodes::ExpressionNode::OperatorType::MINUS);
+                break;
+            case AST::Nodes::AssignOperator::Multiply:
+                newValue = currValue->Operate(newValue, AST::Nodes::ExpressionNode::OperatorType::MULTIPLY);
+                break;
+            case AST::Nodes::AssignOperator::Divide:
+                newValue = currValue->Operate(newValue, AST::Nodes::ExpressionNode::OperatorType::DIVIDE);
+                break;
+            default:
+                assert(false && "Unsupported assign operator met");
+                break;
         }
 
         variable->SetValue(newValue);
