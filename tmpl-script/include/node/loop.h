34116163b4b7430f41017f8fe8140b5fe231a5c9
LandaMm <qwer.009771@gmail.com>
2025-02-23 14:27:08 +0100

    feat: for loop node def

--- tmpl-script/include/node/loop.h
+++ tmpl-script/include/node/loop.h
@@ -29,6 +29,37 @@ namespace AST::Nodes
         inline PNode GetCondition() const { return m_condition; }
         inline PBody GetBody() const { return m_body; }
     };
+
+    class ForLoopNode : public Node
+    {
+    private:
+        using PNode = std::shared_ptr<Node>;
+        using PBody = std::shared_ptr<Statements::StatementsBody>;
+    private:
+        PNode m_decl;
+        PNode m_condition;
+        PNode m_loop_action;
+        PBody m_body;
+
+    public:
+        ForLoopNode(PNode decl, PNode condition, PNode loopAction, PBody body, Location loc)
+            : m_decl(decl),
+              m_condition(condition),
+              m_loop_action(loopAction),
+              m_body(body),
+              Node(loc) { }
+
+	public:
+		inline NodeType GetType() const override { return NodeType::For; };
+		std::string Format() const override;
+        inline bool IsBlock() override { return true; }
+
+    public:
+        inline PNode GetDecl() const { return m_decl; }
+        inline PNode GetCondition() const { return m_condition; }
+        inline PNode GetLoopAction() const { return m_loop_action; }
+        inline PBody GetBody() const { return m_body; }
+    };
 }
 
 #endif // LOOP_NODE_H_
