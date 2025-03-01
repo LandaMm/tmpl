b16043e4dd766b911c0daa388d7656aecf1482c4
LandaMm <qwer.009771@gmail.com>
2025-03-02 00:10:30 +0100

    feat: pass ptr to typechecker

--- tmpl-script/src/typechecker/cast.cpp
+++ tmpl-script/src/typechecker/cast.cpp
@@ -7,7 +7,7 @@ namespace Runtime
         auto target = DiagnoseNode(cast->GetExpr());
         auto targetType = EvaluateType(GetFilename(), cast->GetTypeNode(), m_type_definitions, "TypeError", this);
 
-        return CastType(GetFilename(), target, targetType, cast->GetTypeNode()->GetLocation(), m_type_definitions, "TypeError");
+        return CastType(GetFilename(), target, targetType, cast->GetTypeNode()->GetLocation(), m_type_definitions, "TypeError", this);
     }
 }
 
