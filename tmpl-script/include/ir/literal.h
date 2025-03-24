10d62ba005254d5bddc3981ff725c894ce9f883c
LandaMm <qwer.009771@gmail.com>
2025-03-24 22:02:29 +0100

    feat: move `ConstType` + rename it to `DataType`

--- tmpl-script/include/ir/literal.h
+++ tmpl-script/include/ir/literal.h
@@ -3,30 +3,17 @@
 
 namespace IR
 {
-    enum class ConstType
-    {
-        // Int
-        I8,
-        I16,
-        I32,
-        I64,
-        I128,
-        // Float
-        F32,
-        F64
-    };
-
     class LoadConst : public IRInst, public IRTemp
     {
     private:
-        ConstType m_type;
+        DataType m_type;
         shared_ptr<void> m_value;
     public:
-        LoadConst(tid id, ConstType typ, shared_ptr<void> value)
-            : IRInst(InstType::LoadConst), IRTemp(id),
+        LoadConst(tid id, DataType typ, shared_ptr<void> value)
+            : IRInst(InstType::LoadConst), IRTemp(id, typ),
               m_type(typ), m_value(value) { }
     public:
-        inline ConstType GetType() const { return m_type; }
+        inline DataType GetType() const { return m_type; }
         inline shared_ptr<void> GetValue() const { return m_value; }
     public:
         virtual std::string Format() const override;
