c0a4ae8cee3b271be07eb8966128eb261756532f
LandaMm <qwer.009771@gmail.com>
2025-03-24 22:01:22 +0100

    feat: require data type in IRTempl

--- tmpl-script/include/ir.h
+++ tmpl-script/include/ir.h
@@ -4,12 +4,16 @@
 
 #include "include/node.h"
 #include "include/node/expression.h"
+#include "include/node/function.h"
+#include "include/node/identifier.h"
 #include "include/node/literal.h"
+#include "include/node/logical.h"
 #include "include/node/program.h"
 #include "include/node/statement.h"
 #include "include/parser.h"
 #include<memory>
 #include <string>
+#include <unordered_map>
 
 namespace IR
 {
@@ -36,16 +40,67 @@ namespace IR
         BasicBlock,
     };
 
+    enum class DataType
+    {
+        // Void
+        Void,
+        // Bool
+        I1,
+        // Int
+        I8,
+        I16,
+        I32,
+        I64,
+        I128,
+        // Float
+        F32,
+        F64,
+    };
+
+    inline std::string FormatDataType(DataType typ)
+    {
+        switch(typ)
+        {
+            case DataType::Void:
+                return "void";
+            case DataType::I1:
+                return "i1";
+            case DataType::I8:
+                return "i8";
+            case DataType::I16:
+                return "i16";
+            case DataType::I32:
+                return "i32";
+            case DataType::I64:
+                return "i64";
+            case DataType::I128:
+                return "i128";
+            case DataType::F32:
+                return "f32";
+            case DataType::F64:
+                return "f64";
+            default:
+                break;
+        }
+
+        assert(false && "Should be able to handle all data types for format");
+
+        return "";
+    }
+
     typedef unsigned long tid;
 
     class IRTemp
     {
     private:
         tid m_id;
+        DataType m_dataType;
     public:
-        IRTemp(tid id) : m_id(id) { }
+        IRTemp(tid id, DataType dataType) : m_id(id), m_dataType(dataType) { }
     public:
         inline tid GetId() const { return m_id; }
+        inline DataType GetDataType() const { return m_dataType; }
+        void SetDataType(DataType dataTyp) { m_dataType = dataTyp; }
     public:
         inline std::string Format() const { return "%" + std::to_string(GetId()); }
     };
@@ -68,6 +123,8 @@ namespace IR
     private:
         shared_ptr<ProgramNode> m_program;
         std::string m_filename;
+        std::unordered_map<std::string, DataType> m_fn_dataType;
+        std::unordered_map<std::string, DataType> m_var_dataType;
     private:
         std::vector<shared_ptr<IRInst>> m_parsed;
         tid m_temp_id = 0;
@@ -85,12 +142,21 @@ namespace IR
 
         shared_ptr<IRTemp> ParseExpression(shared_ptr<ExpressionNode> expr);
         shared_ptr<IRTemp> ParseLiteral(shared_ptr<LiteralNode> lit);
+        shared_ptr<IRTemp> ParseIdentifier(shared_ptr<IdentifierNode> id);
+        shared_ptr<IRTemp> ParseFunctionCall(shared_ptr<FunctionCall> fnCall);
+        shared_ptr<IRTemp> ParseCondition(shared_ptr<Condition> cond);
     private:
         void AddInst(shared_ptr<IRInst> inst) { m_parsed.push_back(inst); }
         tid GetTID() { return m_temp_id++; }
     public:
         inline unsigned int GetSize() const { return m_parsed.size(); }
         inline shared_ptr<IRInst> GetInst(unsigned int index) const { return m_parsed[index]; }
+    public: // Functions
+        void RegisterFnReturnType(std::string fnName, DataType dataTyp);
+        DataType* GetFnReturnType(std::string fnName);
+    public: // Variables
+        void RegisterVarType(std::string varName, DataType dataTyp);
+        DataType* GetVarReturnType(std::string varName);
     };
 }
 
