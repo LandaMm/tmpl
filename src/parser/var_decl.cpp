28eef164a3714f426a9892b7b903d72638959299
LandaMm <qwer.009771@gmail.com>
2026-08-01 23:49:37 +0200

    feat: new variable declaration

--- src/parser/var_decl.cpp
+++ src/parser/var_decl.cpp
@@ -6,38 +6,56 @@ namespace AST
 {
 	Node* Parser::VariableDeclaration()
 	{
-        auto keyword = m_lexer->GetToken();
-		bool editable = keyword->GetType() == TokenType::Var;
-        auto varLoc = keyword->GetLocation();
+		Nodes::IdentifierNode* nameNode = Id();
+        auto varLoc = nameNode->GetLocation();
 
-		if (editable)
-			Eat(TokenType::Var);
-		else
-			Eat(TokenType::Const);
+		std::string* name = m_arena.Alloc<std::string>(nameNode->GetName());
+
+		Eat(TokenType::Colon);
 
 		Nodes::TypeNode* type = Type();
 
-		Nodes::IdentifierNode* nameNode = Id();
-		std::string* name = m_arena.Alloc<std::string>(nameNode->GetName());
+		Nodes::SymbolFlag symbolFlag = Nodes::SymbolFlag::NONE;
+		// Symbol Flag
+		// (none) | #foreign
+		if (m_lexer->GetToken()->GetType() == TokenType::Hash)
+		{
+			Eat(TokenType::Hash);
+
+			auto nextToken = m_lexer->GetToken();
+			assert(static_cast<int>(Nodes::SymbolFlag::COUNT_SYMBOL_FLAGS) == 2);
+
+			switch (nextToken->GetType())
+			{
+			case TokenType::Foreign:
+				Eat(TokenType::Foreign);
+
+				symbolFlag = Nodes::SymbolFlag::FOREIGN;
+				break;
+			default:
+			{
+				auto &errManager = GetErrorManager();
+				errManager.UnexpectedToken(m_lexer->GetFilename(), nextToken);
+				break;
+			}
+			}
+		}
 
 		Node* value = nullptr;
 
 		if (m_lexer->GetToken()->GetType() == TokenType::Equal)
 		{
+			// Default values are not allowed for externed symbols
+			if (symbolFlag == Nodes::SymbolFlag::FOREIGN)
+			{
+				// TODO: better error
+				assert(false && "Default value on a foreign variable declaration is not allowed");
+			}
 			Eat(TokenType::Equal);
 			value = Ternary();
 		}
-		else if (!editable)
-		{
-			Prelude::ErrorManager &manager = GetErrorManager();
-			manager.MissingConstantDefinition(GetFilename(), m_lexer->GetToken());
-			return nullptr;
-		}
 
-		if (value != nullptr)
-			return m_arena.Alloc<Nodes::VarDeclaration>(type, name, value, editable, varLoc);
-		else
-			return m_arena.Alloc<Nodes::VarDeclaration>(type, name, varLoc);
+		return m_arena.Alloc<Nodes::VariableDeclaration>(type, name, value, varLoc);
 	}
 }
 
