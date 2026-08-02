14552d98f54de9244efa0c5e775ab80f5f028586
LandaMm <qwer.009771@gmail.com>
2026-08-02 18:36:15 +0200

    feat: parse new `Type` kinds

--- src/parser/type.cpp
+++ src/parser/type.cpp
@@ -1,25 +1,103 @@
+#include <map>
+
 #include "node/type.hpp"
+#include "node/literal.hpp"
 #include "parser.h"
 #include "token.h"
 
 namespace AST
 {
-    Nodes::TypeNode* Parser::Type()
+    Nodes::Type* Parser::Type()
     {
-        auto target = Id();
+        // type_name|*type_name|struct {..}|enum {..}
 
-        auto typ = m_arena.Alloc<Nodes::TypeNode>(target, target->GetLocation());
+        auto token = m_lexer->GetToken();
 
-        return typ;
+        switch (token->GetType())
+        {
+        case TokenType::Id:
+        {
+            auto typeName = Id();
+            return m_arena.Alloc<Nodes::BasicType>(typeName->GetName(), typeName->GetLocation());
+        }
+        case TokenType::Multiply:
+        {
+            Eat(TokenType::Multiply);
+            auto targetType = Type();
+            LocationSpan loc = targetType->GetLocation();
+            loc.begin = token->GetLocation().begin; // include '*' as well
+            return m_arena.Alloc<Nodes::PointerType>(targetType, loc);
+        }
+        case TokenType::Struct:
+        {
+            Eat(TokenType::Struct);
+            Eat(TokenType::OpenCurly);
+
+            // struct {
+            //   name: type;
+            //   ..
+            // }
+            std::map<String, const Nodes::Type*> fields;
+            while (m_lexer->GetToken()->GetType() != TokenType::CloseCurly)
+            {
+                auto fieldName = Id();
+                Eat(TokenType::Colon);
+                auto fieldType = Type();
+                Eat(TokenType::Semicolon);
+                fields.insert({ fieldName->GetName(), fieldType });
+            }
+
+            Eat(TokenType::CloseCurly);
+
+            // TODO: maybe provide loc span of the whole struct including the body
+            auto loc = token->GetLocation();
+            return m_arena.Alloc<Nodes::StructType>(std::move(fields), loc);
+        }
+        case TokenType::Enum:
+        {
+            Eat(TokenType::Enum);
+            Eat(TokenType::OpenCurly);
+
+            // enum {
+            //   | name = value;
+            //   | name;
+            //   ..
+            // }
+            auto enumType = m_arena.Alloc<Nodes::EnumType>(token->GetLocation());
+            while (m_lexer->GetToken()->GetType() != TokenType::CloseCurly)
+            {
+                auto fieldName = Id();
+
+                Uint64 fieldValue = 0;
+                if (m_lexer->GetToken()->GetType() == TokenType::Equal)
+                {
+					Eat(TokenType::Equal);
+                    auto value = Expr();
+                    fieldValue = EvaluateIntegerConstantExpression(value);
+                }
+                else if (enumType->GetFields().Size() > 0)
+                {
+                    fieldValue = enumType->GetFields()[enumType->GetFields().Size() - 1].value + 1;
+                }
+
+                Eat(TokenType::Semicolon);
+
+				enumType->AddField(Nodes::EnumField{ fieldName->GetName(), fieldValue });
+            }
+
+            Eat(TokenType::CloseCurly);
+
+            // TODO: maybe provide loc span of the whole enum including the body
+            auto loc = token->GetLocation();
+            return enumType;
+        }
+        }
+
+        // TODO: better error
+        assert(false && "Invalid type");
     }
 
-    Nodes::TypeNode* Parser::Type(Nodes::IdentifierNode* target)
-    {
-        auto typ = m_arena.Alloc<Nodes::TypeNode>(target, target->GetLocation());
-        return typ;
-    }
-
-    Nodes::CastNode* Parser::Cast(Nodes::TypeNode* typ)
+    Nodes::CastNode* Parser::Cast(Nodes::Type* typ)
     {
         Eat(TokenType::CloseBracket);
 
