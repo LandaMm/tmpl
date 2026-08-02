1c8a9fac845950037ce70b61b9db84fe461294bd
LandaMm <qwer.009771@gmail.com>
2026-08-02 18:34:29 +0200

    fix: use new `Type` and `String

--- include/node/var_declaration.hpp
+++ include/node/var_declaration.hpp
@@ -14,44 +14,44 @@ namespace AST
 		class VariableDeclaration : public Symbol
 		{
 		public:
-			VariableDeclaration(TypeNode* type, std::string* name, Node* value, LocationSpan loc)
-				: m_type(type), m_name(name), m_value(value), Symbol(SymbolType::VARIABLE, SymbolFlag::NONE, loc) { }
+			VariableDeclaration(Type* type, String* name, Node* value, SymbolFlag symbolFlag, LocationSpan loc)
+				: m_type(type), m_name(name), m_value(value), Symbol(SymbolType::VARIABLE, symbolFlag, loc) { }
 
 		public:
 			inline NodeType GetType() const override { return NodeType::VarDecl; }
 
 		public:
-			inline TypeNode* GetType() { return m_type; }
+			inline Type* GetType() { return m_type; }
 			inline Node* GetValue() { return m_value; }
-			inline std::string* GetName() { return m_name; }
+			inline String* GetName() { return m_name; }
 			inline bool HasValue() { return m_value != nullptr; }
 
 		private:
-			TypeNode* m_type;
-			std::string* m_name;
+			Type* m_type;
+			String* m_name;
 			Node* m_value; // nullable
 		};
 
 		class VarDeclaration : public Node
 		{
 		public:
-			VarDeclaration(TypeNode* type, std::string* name, Node* value, bool editable, LocationSpan loc)
+			VarDeclaration(Type* type, std::string* name, Node* value, bool editable, LocationSpan loc)
 				: m_type(type), m_name(name), m_value(value), m_editable(editable), Node(loc) { }
-			VarDeclaration(TypeNode* type, std::string* name, LocationSpan loc)
+			VarDeclaration(Type* type, std::string* name, LocationSpan loc)
 				: m_type(type), m_name(name), m_value(nullptr), m_editable(true), Node(loc) { }
 
 		public:
 			inline NodeType GetType() const override { return NodeType::VarDecl; }
 
 		public:
-			inline TypeNode* GetType() { return m_type; }
+			inline Type* GetType() { return m_type; }
 			inline Node* GetValue() { return m_value; }
 			inline std::string* GetName() { return m_name; }
 			inline bool HasValue() { return m_value != nullptr; }
 			inline bool Editable() { return m_editable; }
 
 		private:
-			TypeNode* m_type;
+			Type* m_type;
 			std::string* m_name;
 			Node* m_value; // nullable
 			bool m_editable;
