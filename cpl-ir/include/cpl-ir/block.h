a79d250c5b95519c886f39b13d65b7236830f8d5
LandaMm <qwer.009771@gmail.com>
2026-08-04 18:16:10 +0200

    feat: IR `BasicBlock` and `NamedBlock` classes

---
+++ cpl-ir/include/cpl-ir/block.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cpl-basics/string.hpp>
+
+#include "instr.h"
+
+#include "cplbuild.h"
+
+namespace IRGenerate
+{
+
+using BlockIdType = Uint32;
+
+class CPL_EXPORT BasicBlock
+{
+public:
+	explicit BasicBlock();
+
+	BasicBlock(const BasicBlock&) = delete;
+	BasicBlock& operator=(const BasicBlock&) = delete;
+	BasicBlock(BasicBlock&&) = default;
+	BasicBlock& operator=(BasicBlock&&) = default;
+public:
+	void AddInstr(Instr* instr);
+private:
+	Array<Instr*> m_body;
+	BlockIdType m_id;
+private:
+	static BlockIdType m_counter;
+};
+
+class CPL_EXPORT NamedBlock : public BasicBlock
+{
+public:
+	explicit NamedBlock(const String &name);
+public:
+	const String& GetName() const noexcept;
+private:
+	String m_name;
+};
+
+} // namespace IRGenerate
