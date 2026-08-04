44e76a8f9767835cf019ec5d81dd553ee48975df
LandaMm <qwer.009771@gmail.com>
2026-08-04 18:17:00 +0200

    feat: IR `BasicBlock` and `Named

---
+++ cpl-ir/src/block.cpp
@@ -0,0 +1,16 @@
+#include "block.h"
+
+namespace IRGenerate
+{
+
+BasicBlock::BasicBlock() : m_id(m_counter++) { }
+
+void BasicBlock::AddInstr(Instr* instr) { m_body.Emplace(instr); }
+
+BlockIdType BasicBlock::m_counter = 0;
+
+NamedBlock::NamedBlock(const String& name) : m_name(name) { }
+
+const String& NamedBlock::GetName() const noexcept { return m_name; }
+
+} // namespace IRGenerate
