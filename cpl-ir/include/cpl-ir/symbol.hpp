c1b07385cfc230e7cd7211d229dbf18dce6e771d
LandaMm <qwer.009771@gmail.com>
2026-08-04 18:15:44 +0200

    feat: IR symbol

---
+++ cpl-ir/include/cpl-ir/symbol.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "cpl-basics/string.hpp"
+
+namespace IRGenerate
+{
+
+enum class SymbolType
+{
+	UNKNOWN = 0,
+	VARIABLE,
+	FUNCTION,
+	COUNT_SYMBOL_TYPES,
+};
+
+enum class SymbolOrigin
+{
+	UNKNOWN = 0,
+	LOCAL,
+	FOREIGN,
+	COUNT_SYMBOL_ORIGINS,
+};
+
+class Symbol
+{
+public:
+	explicit Symbol(SymbolType typ, SymbolOrigin origin, const String &name)
+		: m_type(typ), m_origin(origin), m_name(name) { }
+public:
+	SymbolType Type() const noexcept { return m_type; }
+	SymbolOrigin Origin() const noexcept { return m_origin; }
+	const String& Name() const { return m_name; }
+private:
+	SymbolType m_type;
+	SymbolOrigin m_origin;
+	String m_name;
+};
+
+} // namespace IRGenerate
