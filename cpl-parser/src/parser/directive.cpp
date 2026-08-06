60655e390e9ff94f919cabcc36335a89070d4541
LandaMm <qwer.009771@gmail.com>
2026-08-06 18:04:10 +0200

    feat: evaluate `ImportDirective` at parsing stage

--- cpl-parser/src/parser/directive.cpp
+++ cpl-parser/src/parser/directive.cpp
@@ -1,8 +1,8 @@
+#include "cpl-parser/node/directive.hpp"
 
-
+#include <filesystem>
 #include <memory>
 #include "cpl-parser/parser.h"
-#include "cpl-parser/node/directive.hpp"
 
 namespace AST
 {
@@ -10,15 +10,37 @@ namespace AST
 
     Node* Parser::ImportStatement()
     {
-        auto loc = Current()->GetLocation();
-        Eat(TokenType::Import);
+		auto loc = Current()->GetLocation();
+		Eat(TokenType::Import);
 
-        auto token = Current();
-        Eat(TokenType::String);
-        auto module = token->GetValue<String>();
-        assert(module);
+		auto token = Current();
+		Eat(TokenType::String);
+		auto module = token->GetValue<String>();
+		assert(module);
 
-        return m_arena.Alloc<ImportDirective>(*module, loc);
+		std::filesystem::path modulePath = GetFilename().c_str();
+		modulePath = modulePath.parent_path() / module->c_str();
+
+		auto importDirective = m_arena.Alloc<ImportDirective>(*module, loc);
+
+		try {
+			FileReader reader(modulePath.string().c_str());
+
+			if (!reader.Ok())
+			{
+				throw std::runtime_error("file failed to open");
+			}
+
+			Lexer lexer(reader, module->c_str(), m_lexer->Allocator());
+			lexer.Tokenize();
+
+			m_lexer->MergeLexer(lexer);
+		} catch (...) {
+			GetErrorManager().FailedOpeningFile(module->c_str());
+			return importDirective;
+		}
+
+		return importDirective;
     }
 
     Node* Parser::ExternStatement()
