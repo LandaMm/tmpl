ae59206536cb3822ae0ea43bf73d47ae83ef3c90
LandaMm <qwer.009771@gmail.com>
2026-08-07 19:02:28 +0200

    feat: IR extend `CallInstr` with arguments and ret

--- cpl-ir/include/cpl-ir/instr/call.hpp
+++ cpl-ir/include/cpl-ir/instr/call.hpp
@@ -3,6 +3,8 @@
 #include "cpl-ir/instr.h"
 #include "cpl-ir/symbol.hpp"
 
+#include "cpl-ir/value.hpp"
+
 #include "cplbuild.h"
 
 namespace IRGenerate
@@ -11,12 +13,16 @@ namespace IRGenerate
 class CPL_EXPORT CallInstr : public Instr
 {
 public:
-	explicit CallInstr(const Symbol &callSymbol)
-		: m_callSymbol(callSymbol), Instr(InstrOp::CALL) { }
+	explicit CallInstr(const Symbol &callSymbol, Array<const Value*>&& args, const IRGenerate::Type* retType)
+		: m_callSymbol(callSymbol), m_args(args), m_retType(retType), Instr(InstrOp::CALL) {}
 public:
-	const Symbol& CallSymbol() const noexcept { return m_callSymbol; }
+	inline const Symbol& CallSymbol() const noexcept { return m_callSymbol; }
+	inline const Array<const Value*>& Args() const noexcept { return m_args; }
+	inline const IRGenerate::Type* RetType() const noexcept { return m_retType; }
 private:
 	Symbol m_callSymbol;
+	const IRGenerate::Type* m_retType;
+	Array<const Value*> m_args;
 };
 
 } // namespace IRGenerate
