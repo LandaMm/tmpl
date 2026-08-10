e7f6c17401fa1cb8d9263b584a94ed28fe4fd4b6
LandaMm <qwer.009771@gmail.com>
2026-08-10 20:10:08 +0200

    feat: IR `AllocaInstr`

---
+++ cpl-ir/include/cpl-ir/instr/alloca.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "cpl-ir/instr.h"
+#include "cpl-ir/symbol.hpp"
+
+#include "cpl-ir/value.hpp"
+
+#include "cplbuild.h"
+
+namespace IRGenerate
+{
+
+class CPL_EXPORT AllocaInstr : public Instr
+{
+public:
+	explicit AllocaInstr(const Value* dest, const IRGenerate::Type* typ)
+		: m_typ(typ), Instr(InstrOp::ALLOCA, dest) {}
+public:
+	inline const IRGenerate::Type* Typ() const noexcept { return m_typ; }
+private:
+	const IRGenerate::Type* m_typ;
+};
+
+} // namespace IRGenerate
