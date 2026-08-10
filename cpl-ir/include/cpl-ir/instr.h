8e9032594f619e495938fb9114cdb4f9fd2c4085
LandaMm <qwer.009771@gmail.com>
2026-08-10 20:12:33 +0200

    feat: IR `Instr` can hold destination save value + new `InstrOp` enum fields

--- cpl-ir/include/cpl-ir/instr.h
+++ cpl-ir/include/cpl-ir/instr.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "value.hpp"
+
 #include "cplbuild.h"
 
 namespace IRGenerate
@@ -9,13 +11,16 @@ enum class InstrOp
 {
 	NONE = 0,
 	CALL,
+	ALLOCA,
+	STORE,
 	LOAD,
+	COUNT_INSTR_OPS,
 };
 
 class CPL_EXPORT Instr
 {
 public:
-	explicit Instr(InstrOp op);
+	explicit Instr(InstrOp op, const Value* dst = nullptr);
 	virtual ~Instr() = default;
 
 	Instr(const Instr&) = default;
@@ -23,9 +28,12 @@ public:
 	Instr(Instr&&) = delete;
 	Instr& operator=(Instr&&) = delete;
 public:
+	inline bool Valued() const noexcept { return m_dst != nullptr; }
+	inline const Value* Dest() const noexcept { return m_dst; }
 	InstrOp Type() const noexcept;
 private:
 	InstrOp m_op;
+	const Value* m_dst;
 };
 
 } // namespace IRGenerate
