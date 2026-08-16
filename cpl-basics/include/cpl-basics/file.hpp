8d52bbc92982957e8bdbaff4c7aa34d4eacbfdb0
LandaMm <qwer.009771@gmail.com>
2026-08-16 16:19:34 +0200

    feat: basics file output stream s

--- cpl-basics/include/cpl-basics/file.hpp
+++ cpl-basics/include/cpl-basics/file.hpp
@@ -53,3 +53,35 @@ public:
 private:
 	std::ifstream m_file;
 };
+
+class FileStreamWriter
+{
+public:
+	FileStreamWriter(const char* path) : m_file(path, std::ios::binary)
+	{
+		if (!m_file.is_open())
+		{
+			throw new std::runtime_error((String("failed to open file at ") + path).c_str());
+		}
+	}
+
+	~FileStreamWriter()
+	{
+		m_file.close();
+	}
+
+	FileStreamWriter(const FileStreamWriter&) = delete;
+	FileStreamWriter& operator=(const FileStreamWriter&) = delete;
+	FileStreamWriter(FileStreamWriter&&) = delete;
+	FileStreamWriter& operator=(FileStreamWriter&&) = delete;
+public:
+	inline bool Ok() const { return m_file.good(); }
+
+	void Write(const String& what)
+	{
+		m_file << what;
+	}
+private:
+	std::ofstream m_file;
+};
+
